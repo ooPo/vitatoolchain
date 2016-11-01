c012ceabcb2a85e2723b387180401edf3249c813
Sunguk Lee <d3m3vilurr@gmail.com>
2016-11-02 00:35:27 +0900

    Fix build problems of openssl & curl

--- scripts/011-vita-curl.sh
+++ scripts/011-vita-curl.sh
@@ -1,16 +1,13 @@
 #!/bin/sh -e
 # vita-curl.sh by Sunguk Lee (d3m3vilurr@gmail.com)
 
-## This didn't build for me, disabling for now.
-exit
-
 VITA_CURL="vita-curl"
 BRANCH="vita"
 
 if [ ! -d ${VITA_CURL}-${BRANCH} ]; then
 
   ## Download the source code.
-  wget --continue https://github.com/xyzz/${VITA_CURL}/archive/${BRANCH}.tar.gz -O ${VITA_CURL}-${BRANCH}.tar.gz
+  wget --continue https://github.com/d3m3vilurr/${VITA_CURL}/archive/${BRANCH}.tar.gz -O ${VITA_CURL}-${BRANCH}.tar.gz
 
   ## Unpack the source code.
   tar xfvz ${VITA_CURL}-${BRANCH}.tar.gz
@@ -22,25 +19,8 @@ cd ${VITA_CURL}-${BRANCH}
 
 ## Configure the build.
 ./buildconf
-./configure --host=arm-vita-eabi \
-    --with-ssl=${VITASDK}/arm-vita-eabi/ \
-    --disable-shared \
-    --disable-ftp \
-    --disable-ldap \
-    --disable-imap \
-    --disable-ipv6 \
-    --disable-rtsp \
-    --disable-dict \
-    --disable-file \
-    --disable-gopher \
-    --disable-pop3 \
-    --disable-smtp \
-    --disable-telnet \
-    --disable-tftp \
-    --enable-https \
-    --disable-smb \
-    --disable-smbs \
-    --prefix=${VITASDK}/arm-vita-eabi/
+./configure --host=arm-vita-eabi  --prefix=${VITASDK}/arm-vita-eabi/
+cp lib/config-vita.h lib/curl_config.h
 
 ## Compile and install.
 ${MAKE:-make} -j4 && ${MAKE:-make} install
